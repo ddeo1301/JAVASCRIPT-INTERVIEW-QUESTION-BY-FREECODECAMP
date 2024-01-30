271a1e740a556333b5fb54281c197218b8cd8068
ddeo1301 <divyanshudeo1301@gmail.com>
2024-01-30 10:33:52 +0530

    some more

---
+++ 20AuthenticateAndAuthorization.js
@@ -0,0 +1,6 @@
+// https://codewithpawan.medium.com/authentication-and-authorization-in-node-js-a-comprehensive-guide-2755b57dce27
+
+/// https://vegibit.com/authentication-and-authorization-in-node-js/
+
+
+// 
--- 6Currying.js
+++ 6Currying.js
@@ -12,7 +12,6 @@ console.log(f(5)(6));  // 5  6
 
 
 // why should we use currying
-
 // It helps us to create a higher-order function
 // reduces the chances of error in our function by dividing it into multiple smaller functions that can handle one responsibility.
 // useful in building modular and reusable code
@@ -20,3 +19,30 @@ console.log(f(5)(6));  // 5  6
 // code more readable
 
 
+// Some commonly used timing features of Node.js include:
+//setTimeout: Executes a callback function after a specified delay.
+//setInterval: Repeatedly executes a callback function with a fixed time interval.
+//setImmediate: Executes a callback function immediately after the current event loop iteration completes.
+//process.nextTick: Defers the execution of a callback function until the next iteration of the event loop.
+
+
+// Abstraction: Hiding unnecessary details and exposing relevant information. It simplifies complex systems
+//  by providing a generalized view.
+// Encapsulation: Bundling data and methods into a class, controlling access to them. It ensures data
+//  integrity and hides implementation details.
+
+
+// DELETE: Removes specific rows from a table based on conditions. It is a logged operation and can be
+//  rolled back.
+// TRUNCATE: Removes all rows from a table. It is faster than DELETE, deallocates space, and resets table
+//  identity values. TRUNCATE cannot be rolled back as it is not logged.
+
+
+// The different HTTP methods are:
+// 1. GET: Used to retrieve data from a server. It should be used when you want to fetch information without
+//  making any modifications.
+// 2. POST: Used to send data to a server to create a new resource. It should be used when you want to 
+// submit data to the server, like submitting a form.
+// 3. PUT: Used to send data to a server to update an existing resource. It should be used when you want
+//  to modify an existing resource completely.
+// 4. DELETE: Used to delete a specified resource on the server.'
